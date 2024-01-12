bab0e1a77b11b60b271d6a304d84aa3e604b5b43
PSKutergin <PSKutergin@gmail.com>
2024-01-12 16:24:20 +0300

    получение данных с бэка и отрисовка карточек товаров

--- src/components/CardList.vue
+++ src/components/CardList.vue
@@ -3,14 +3,20 @@ import Card from './Card.vue'
 
 const onClickAdd = () => alert('Добавлено в корзину')
 const onClickFavourite = () => alert('Добавлено в избранное')
+
+defineProps({
+  items: Array
+})
 </script>
 
 <template>
   <div class="grid grid-cols-4 gap-5">
     <Card
-      imageUrl="/sneakers/sneakers-1.jpg"
-      title="Мужские Кроссовки Nike Blazer Mid Suede"
-      :price="5000"
+      v-for="item in items"
+      :key="item.id"
+      :imageUrl="item.imageUrl"
+      :title="item.title"
+      :price="item.price"
       :isAdded="false"
       :isFavourite="false"
       :onClickAdd="onClickAdd"
