6160b07cb5400f03561a765ccc3267f4490ff4bd
PSKutergin <PSKutergin@gmail.com>
2024-01-12 17:18:51 +0300

    сортировка данных и поиск по названию

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, ref } from 'vue'
+import { onMounted, reactive, ref, watch } from 'vue'
 import axios from 'axios'
 
 import Header from './components/Header.vue'
@@ -7,15 +7,40 @@ import CardList from './components/CardList.vue'
 // import Drawer from './components/Drawer.vue'
 
 const items = ref([])
+const filters = reactive({
+  sortBy: 'title',
+  searchQuery: ''
+})
 
-onMounted(async () => {
+const onChangeSelect = (event) => {
+  filters.sortBy = event.target.value
+}
+
+const onChangeSearchInput = (event) => {
+  filters.searchQuery = event.target.value
+}
+
+const fetchItems = async () => {
   try {
-    const { data } = await axios.get('https://9c50affb1ee38946.mokky.dev/items')
+    const params = {
+      sortBy: filters.sortBy
+    }
+
+    if (filters.searchQuery) {
+      params.title = `*${filters.searchQuery}*`
+    }
+
+    const { data } = await axios.get(`https://9c50affb1ee38946.mokky.dev/items`, {
+      params
+    })
     items.value = data
   } catch (error) {
     console.log(error)
   }
-})
+}
+
+onMounted(fetchItems)
+watch(filters, fetchItems)
 </script>
 
 <template>
@@ -28,15 +53,16 @@ onMounted(async () => {
         <h2 class="text-3xl font-bold mb-8">Все кроссовки</h2>
 
         <div class="flex gap-4">
-          <select class="py-2 px-3 rounded-md outline-none">
-            <option>По названию</option>
-            <option>По цене (возрастанию)</option>
-            <option>По цене (убыванию)</option>
+          <select class="py-2 px-3 rounded-md outline-none" @change="onChangeSelect">
+            <option value="name">По названию</option>
+            <option value="price">По цене (возрастанию)</option>
+            <option value="-price">По цене (убыванию)</option>
           </select>
 
           <div class="relative">
             <img src="/search.svg" alt="Search" class="absolute top-3 left-3" />
             <input
+              @input="onChangeSearchInput"
               type="text"
               class="border rounded-md pr-4 pl-10 py-2 outline-none focus:border-gray-400"
               placeholder="Поиск..."
