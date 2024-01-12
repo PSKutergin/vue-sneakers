93b8789ab14f1390217356f321788c99c87f420e
PSKutergin <PSKutergin@gmail.com>
2024-01-12 23:39:08 +0300

    отображение пустой корзины

--- src/components/Drawer.vue
+++ src/components/Drawer.vue
@@ -1,6 +1,7 @@
 <script setup>
 import DrawerHead from './DrawerHead.vue'
 import CartItemList from './CartItemList.vue'
+import InfoBlock from './InfoBlock.vue'
 
 const emit = defineEmits(['createOrder'])
 
@@ -15,28 +16,39 @@ defineProps({
   <div class="fixed top-0 left-0 h-full w-full bg-black z-10 opacity-70"></div>
   <div class="fixed top-0 right-0 h-full w-96 bg-white z-20 p-8">
     <DrawerHead />
-    <CartItemList />
 
-    <div class="flex flex-col gap-4 mt-7">
-      <div class="flex gap-2">
-        <span>Итого:</span>
-        <div class="flex-1 border-b border-dashed"></div>
-        <b>{{ totalPrice }} руб.</b>
+    <div v-if="!totalPrice" class="flex h-full items-center">
+      <InfoBlock
+        title="Корзина пустая"
+        description="Добавьте хотя бы одну пару кроссовок, чтобы сделать заказ."
+        imageUrl="/package-icon.png"
+      />
+    </div>
+
+    <div v-else>
+      <CartItemList />
+
+      <div class="flex flex-col gap-4 mt-7">
+        <div class="flex gap-2">
+          <span>Итого:</span>
+          <div class="flex-1 border-b border-dashed"></div>
+          <b>{{ totalPrice }} руб.</b>
+        </div>
+
+        <div class="flex gap-2">
+          <span>Налог 5%:</span>
+          <div class="flex-1 border-b border-dashed"></div>
+          <b>{{ vatPrice }} руб.</b>
+        </div>
+
+        <button
+          @click="() => emit('createOrder')"
+          :disabled="buttonDisabled"
+          class="mt-4 bg-lime-500 text-white rounded-xl py-3 w-full transition disabled:bg-slate-300 hover:bg-lime-600 active:bg-lime-700 cursor-pointer"
+        >
+          Оформить заказ
+        </button>
       </div>
-
-      <div class="flex gap-2">
-        <span>Налог 5%:</span>
-        <div class="flex-1 border-b border-dashed"></div>
-        <b>{{ vatPrice }} руб.</b>
-      </div>
-
-      <button
-        @click="() => emit('createOrder')"
-        :disabled="buttonDisabled"
-        class="mt-4 bg-lime-500 text-white rounded-xl py-3 w-full transition disabled:bg-slate-300 hover:bg-lime-600 active:bg-lime-700 cursor-pointer"
-      >
-        Оформить заказ
-      </button>
     </div>
   </div>
 </template>
